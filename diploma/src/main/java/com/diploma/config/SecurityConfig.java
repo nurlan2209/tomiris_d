779286d8b29047851b0d998ad190f2383024ff5f
nurlan <nkuanyshuly2020@gmail.com>
2025-05-13 22:32:47 +0500

    gotov

--- diploma/src/main/java/com/diploma/config/SecurityConfig.java
+++ diploma/src/main/java/com/diploma/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -38,6 +39,7 @@ public class SecurityConfig {
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                         .permitAll()
                 );
         return http.build();
